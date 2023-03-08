666264c911c17ae0867a6663828d0828657a6d24
PedroChaps <pedro.chaparro@tecnico.ulisboa.pt>
2023-03-08 01:05:45 +0000

    most server implemented; Mussing services

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -1,9 +1,51 @@
 package pt.tecnico.distledger.server;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import pt.tecnico.distledger.server.domain.service.AdminServiceImpl;
+import pt.tecnico.distledger.server.domain.service.UserServiceImpl;
+
+import java.io.IOException;
+
 public class ServerMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println(ServerMain.class.getSimpleName());
 
-        /* TODO */
+        // Receive and print arguments
+        System.out.printf("Received %d arguments%n", args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.printf("arg[%d] = %s%n", i, args[i]);
+        }
+
+        // Checks arguments
+        if (args.length < 1) {
+            System.err.println("Argument(s) missing!");
+            System.err.printf("Usage: java %s port%n", ServerMain.class.getName());
+            return;
+        }
+
+        // Converts the arguments
+        final int port = Integer.parseInt(args[0]);
+        final String qualifier = args[1];
+        // TODO: read debug command and set debug mode
+
+        // Creates the services
+        final BindableService userImpl = new UserServiceImpl();
+        final BindableService adminImpl = new AdminServiceImpl();
+
+        // Create a new server to listen on port
+        Server server = ServerBuilder.forPort(port).addService(userImpl).addService(adminImpl).build();
+
+        // Start the server
+        server.start();
+
+        // Server threads are running in the background.
+        System.out.println("Server started");
+
+        // Do not exit the main thread. Wait until server is terminated.
+        server.awaitTermination();
 
     }
 
