df9acda260114d5eb01e5bd9d740d1dc32ec7acd
Bernardo Prata <bernardo.almeida.prata@tecnico.ulisboa.pt>
2023-03-09 20:34:20 +0000

    Implemented gRPC client-side logic

--- User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
+++ User/src/main/java/pt/tecnico/distledger/userclient/grpc/UserService.java
@@ -1,8 +1,46 @@
 package pt.tecnico.distledger.userclient.grpc;
 
-public class UserService {
+import pt.tecnico.distledger.contract.user.UserDistLedger.*;
+import pt.tecnico.distledger.contract.user.*;
 
-    /*TODO: The gRPC client-side logic should be here.
-        This should include a method that builds a channel and stub,
-        as well as individual methods for each remote operation of this service. */
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+
+public class UserService implements AutoCloseable{
+    private final ManagedChannel channel;
+    private final UserServiceGrpc.UserServiceBlockingStub stub;
+
+    public UserService(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
+
+    public UserService(ManagedChannelBuilder<?> channelBuilder) {
+        channel = channelBuilder.build();
+        stub = UserServiceGrpc.newBlockingStub(channel);
+    }
+
+    public CreateAccountResponse createAccount(CreateAccountRequest request) {
+        return stub.createAccount(request);
+    }
+
+    public DeleteAccountResponse deleteAccount(DeleteAccountRequest request){
+        return stub.deleteAccount(request);
+    }
+
+
+    public BalanceResponse getBalance(BalanceRequest request) {
+        return stub.balance(request);
+    }
+
+    public  TransferToResponse transferTo(TransferToRequest request ){
+        return stub.transferTo(request);
+    }
+
+    @Override
+    public final void close() {
+        channel.shutdown();
+    }
 }
+
+
