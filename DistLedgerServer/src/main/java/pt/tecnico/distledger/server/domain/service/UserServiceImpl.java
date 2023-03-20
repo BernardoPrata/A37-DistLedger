f2ee83d9424561919e7e66a18a4ad35e73f95f81
PedroChaps <pedro.chaparro@tecnico.ulisboa.pt>
2023-03-20 17:41:10 +0000

    fix: change Exception's status

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/service/UserServiceImpl.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/service/UserServiceImpl.java
@@ -39,7 +39,7 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
             responseObserver.onCompleted();
         }
         catch (DistLedgerServerException e) {
-            responseObserver.onError(ALREADY_EXISTS.withDescription(e.getMessage()).asRuntimeException());
+            responseObserver.onError(INVALID_ARGUMENT.withDescription(e.getMessage()).asRuntimeException());
         }
     }
 
@@ -68,7 +68,7 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
             responseObserver.onCompleted();
         }
         catch (DistLedgerServerException e) {
-            responseObserver.onError(ABORTED.withDescription(e.getMessage()).asRuntimeException());
+            responseObserver.onError(INVALID_ARGUMENT.withDescription(e.getMessage()).asRuntimeException());
         }
     }
 
