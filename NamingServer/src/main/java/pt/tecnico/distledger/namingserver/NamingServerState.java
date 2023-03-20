39f7e103f0802ca95312332004073e1cc5b923b0
PedromcaMartins <pedromcamartins@tecnico.ulisboa.pt>
2023-03-20 16:23:40 +0000

    lookup implementado no User e melhorias no NS debugs improved naming server User agora recebe address do NS como argumento e cria novo NS service lookup chamado antes de executar comandos user service atualiza o channel com o server a cada comando (lookup)

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServerState.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServerState.java
@@ -1,7 +1,6 @@
 package pt.tecnico.distledger.namingserver;
 
 import pt.tecnico.distledger.namingserver.domain.ServiceEntry;
-
 import pt.tecnico.distledger.namingserver.domain.ServerEntry;
 
 import java.util.HashMap;
@@ -16,6 +15,20 @@ public class NamingServerState {
     public NamingServerState(boolean toDebug) {
         this.serviceEntries = new HashMap<String, ServiceEntry>();
         this.toDebug = toDebug;
+
+        // TODO: remover
+        ServiceEntry serviceEntry1 = new ServiceEntry("createAccount");
+        serviceEntry1.addServerEntry(new ServerEntry("localhost:50051", "A"));
+        ServiceEntry serviceEntry2 = new ServiceEntry("transferTo");
+        serviceEntry2.addServerEntry(new ServerEntry("localhost:50051", "A"));
+        ServiceEntry serviceEntry3 = new ServiceEntry("deleteAccount");
+        serviceEntry3.addServerEntry(new ServerEntry("localhost:50051", "A"));
+        ServiceEntry serviceEntry4 = new ServiceEntry("balance");
+        serviceEntry4.addServerEntry(new ServerEntry("localhost:50051", "A"));
+        addServiceEntry(serviceEntry1);
+        addServiceEntry(serviceEntry2);
+        addServiceEntry(serviceEntry3);
+        addServiceEntry(serviceEntry4);
     }
 
     private void debug(String debugMessage) {
@@ -44,13 +57,16 @@ public class NamingServerState {
     }
 
     public List<String> lookup(String serviceName, String qualifier) {
-        ServiceEntry serviceEntry = this.serviceEntries.get(serviceName);
+        ServiceEntry serviceEntry = getServiceEntry(serviceName);
         if (serviceEntry == null) {
-            debug("Service " + serviceName + " not found");
+            debug("lookup: Service " + serviceName + " not found");
             return new ArrayList<String>();
         }
 
-        return serviceEntry.lookup(qualifier);
+        List<String> serviceEntriesWithQualifier = serviceEntry.lookup(qualifier);
+        debug("lookup: Service " + serviceName + " found in servers: " + serviceEntriesWithQualifier);
+
+        return serviceEntriesWithQualifier;
     }
 
 }
